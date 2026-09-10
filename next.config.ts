f29bd61695797e493838c95af677789ca9ca4dd0
Visarut Sankham <visarut298@gmail.com>
2026-09-10 14:13:37 +0700

    Use asset prefix for static single page export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/yangnar-project-management-demo' : '',
+  assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/yangnar-project-management-demo' : '',
 };
 
 export default nextConfig;
